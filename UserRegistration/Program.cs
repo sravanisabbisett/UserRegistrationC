0a51246a37ac57c8737ae44d83228bc8cdbf9fd3
sravanisabbisett <sravani.sabbisetti1@gmail.com>
2020-11-26 15:57:12 +0530

    ADDED:Number pattern to validate the user mobiulrnumber

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -8,12 +8,15 @@ namespace UserRegistration
         {
             Console.WriteLine("Welcome to user Registration problem");
             Validation validation = new Validation();
-            Console.WriteLine("Enter your Firstname");
+            /*Console.WriteLine("Enter your Firstname");
             string firstName = Console.ReadLine();
             Console.WriteLine(validation.FirstName_Validation(firstName));
             Console.WriteLine("Enter your Lastname");
             string lastName = Console.ReadLine();
-            Console.WriteLine(validation.FirstName_Validation(lastName));
+            Console.WriteLine(validation.FirstName_Validation(lastName));*/
+            Console.WriteLine("Enter your Mobilenumber");
+            string number = Console.ReadLine();
+            Console.WriteLine(validation.Number_Validation(number));
             Console.ReadKey();
         }
     }
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -8,12 +8,19 @@ namespace UserRegistration
     public class Validation
     {
         public const string NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-        
+        public const string MOBILENUMBER_PATTERN = "^[0-9]{2}\\s[6-9]{1}[0-9]{9}$";
 
         public bool FirstName_Validation(string name)
         {
             if (Regex.IsMatch(name, NAME_PATTERN))
                return true;
+            return false;
+        }
+
+        public bool Number_Validation(string number)
+        {
+            if (Regex.IsMatch(number, MOBILENUMBER_PATTERN))
+                return true;
             return false;
         }
     }
